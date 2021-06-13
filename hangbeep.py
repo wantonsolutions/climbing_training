9c1cb36938d18016bad0275093f79633bdab9137
wantonsolutions <stewbertgrant@gmail.com>
2021-06-13 00:13:26 -0700

    added warmup

--- hangbeep.py
+++ hangbeep.py
@@ -22,11 +22,16 @@ def beep():
 
 
 def count_down(duration):
+	#don't count right away
+	hold_first_n=2
+	full_duration=duration
 	speak_interval=10
 	start_count_down=3
 	while duration > 0:
 		sduration=str(duration)
-		if duration % speak_interval == 0:
+		if full_duration-duration < hold_first_n and duration > start_count_down:
+			print(sduration)
+		elif duration % speak_interval == 0:
 			speak(sduration, block=False)
 		elif duration <= start_count_down:
 			speak(sduration, block=False)
@@ -38,8 +43,9 @@ def count_down(duration):
 
 
 
-def individual_finger(finger, rest_interval, active_interval):
-	duration = timed_speak(finger + " finger")
+def individual_finger(finger, active_interval, rest_interval,):
+	duration = timed_speak(finger)
+	duration += timed_speak("Hold for " + str(active_interval) + "seconds")
 	count_down(rest_interval - duration)
 
 	speak("Start", block=False)
@@ -48,55 +54,76 @@ def individual_finger(finger, rest_interval, active_interval):
 
 	speak("Rest")
 
+def start_example():
+	speak("Start on beep sound ")
+	speak("example start beep")
+	time.sleep(0.5)
+	beep()
+	time.sleep(1)
+
+def describe_workout(workout_name, fingers, sets, set_rest, active_interval, rest_interval):
+	speak("Running finger workout protocol "+ workout_name)
+	speak("Active interval for each exercise " + str(active_interval) + " seconds")
+	speak("Rest interval " + str(rest_interval) + " seconds")
+	speak("Rest between sets " + str(set_rest) + " seconds")
+
+	active_set_time = ((active_interval+rest_interval) * len(fingers))
+	workout_time = (active_set_time * sets) + (set_rest * (sets-1))
+	workout_mins = int((workout_time / 60))
+	speak("Estemated workout time " + str(workout_mins) + " minutes")
+
 
 
 def exercise_fingers(fingers, active_interval,rest_interval):
 	for finger in fingers:
-		individual_finger(finger,active_interval,rest_interval)
-
-def finger_workout():
-	sets=5
-	set_rest=60
-	active_interval=5
-	rest_interval=5
-	fingers = ["pointer", "middle", "ring", "pinkey"]
-
-	preamble=False
+		#this just prevents a rest from running on the first iteration
+		rest=rest_interval
+		if finger == fingers[0]:
+			rest=5
+		individual_finger(finger,active_interval,rest)
 
 
+def workout(workout_name,fingers,sets, set_rest, active_interval, rest_interval, preamble=True):
 	if preamble:
-		speak("Preparing finger work out")
-		speak("Hang individual fingers on command")
-		speak("Hang interval for each finger " + str(active_interval) + " seconds")
-		speak("Rest interval " + str(rest_interval) + " seconds")
-		speak("Rest between sets " + str(set_rest) + " seconds")
-
-		active_set_time = ((active_interval+rest_interval) * len(fingers))
-		workout_time = (active_set_time * sets) + (set_rest * (sets-1))
-		workout_mins = int((workout_time / 60))
-		speak("Estemated workout time " + str(workout_mins) + " minutes")
-
-		speak("Start on beep sound ")
-		speak("example start beep")
-		time.sleep(0.5)
-		beep()
-		time.sleep(1)
-		speak("Beginning workout")
-
-
+		describe_workout(workout_name,fingers,sets,set_rest, active_interval, rest_interval)
+		start_example()
+	
+	
+	speak("Beginning workout " + workout_name)
 	
 	for i in range(sets):
 		speak("Set Number "+ str(i) +" Prepare for set")
 		exercise_fingers(fingers, active_interval, rest_interval)
 
 		if i < sets-1:
-			duration = timed_speak("Set " + str(i) + " out of " + str(sets) +" complete. Rest between sets " + str(set_rest) + " seconds")
+			duration = timed_speak("Set " + str(i) + " out of " + str(sets-1) +" complete. Rest between sets " + str(set_rest) + " seconds")
 			count_down(set_rest - duration)
-	
+
 	time.sleep(1)
 	speak("Finger board exercise complete")
 	speak("Good Job Stew")
 
 
+def warmup():
+	sets=3
+	set_rest=30
+	active_interval=10
+	rest_interval=10
+	fingers=[ "All four fingers", "All four fingers", "First Two Fingers", "Last Two fingers"]
+	workout_name="warm up"
+	preamble=False
+	workout(workout_name,fingers,sets,set_rest,active_interval,rest_interval,preamble)
 
-finger_workout()
+
+def mono_training():
+	sets=5
+	set_rest=60
+	active_interval=5
+	rest_interval=5
+	fingers = ["pointer finger", "middle finger", "ring finger", "pinkey finger"]
+	workout_name="mono training"
+	workout(workout_name, fingers,sets,set_rest,active_interval,rest_interval)
+
+
+warmup()
+mono_training()
